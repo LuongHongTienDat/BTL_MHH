5b119456e9dce1ed33bd3496859f803e8b687f29
Hoang39 <87194190+Hoang39@users.noreply.github.com>
2021-11-14 17:49:31 +0700

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
+from tkinter import scrolledtext
 
 window = tk.Tk()
+window.resizable(False, False)
 
 window.title("The patients' Petri Net")
 frame = Frame(window, bd = 10, bg = "silver")
@@ -14,11 +16,11 @@ frame_input.pack(side = LEFT)
 
 canvas = tk.Canvas(frame, bd = 1, height = 250, width = 1000)
 canvas.pack()
-canvas2 = tk.Canvas(frame2, bd = 1, height = 300, width = 500)
+canvas2 = tk.Canvas(frame2, bd = 1, height = 280, width = 500)
 canvas2.pack()
-canvas_input = tk.Canvas(frame_input, bd = 1, height = 300, width = 500)
+canvas_input = tk.Canvas(frame_input, bd = 1, height = 280, width = 500)
 canvas_input.pack()
-window.geometry("1000x550")
+window.geometry("1000x530")
 
 def create_circle(x, y, r, canvasName):
     x0 = x - r
@@ -41,52 +43,74 @@ canvas.create_text(500, 140, fill = "dimgrey", font = "Arial 14", text = "inside
 canvas.create_text(900, 140, fill = "dimgrey", font = "Arial 14", text = "done")
 
 def startclick():
-    if wait.get("1.0", END) == "\n":
-        messagebox.showerror("Error!!!", "Please input before firing.")
-        return 
-    
     wait.config(state = "normal")
     inside.config(state = "normal")
 
     a = int(wait.get("1.0", END))
-    b = int(inside.get("1.0", END))
 
     if a == 0:
-        messagebox.showerror("Error!!!", "There are no tokens in the place wait.")
+        messagebox.showerror("Error!!!", "There are no tokens in place [wait].")
         return
+
+    b = int(inside.get("1.0", END))
+    c = int(done.get("1.0", END))
 
     wait.delete("1.0", END)
     inside.delete("1.0", END)
 
-    wait.insert(END, a - 1)
-    inside.insert(END, b + 1)
+    a = a - 1
+    b = b + 1
+
+    wait.tag_configure("tag_name", justify='center')
+    wait.insert(END, a)
+    wait.tag_add("tag_name", "1.0", "end")
+
+    inside.tag_configure("tag_name", justify='center')
+    inside.insert(END, b)
+    inside.tag_add("tag_name", "1.0", "end")
 
     wait.config(state = "disabled")
     inside.config(state = "disabled")
 
-def changeclick():
-    if inside.get("1.0", END) == "\n":
-        messagebox.showerror("Error!!!", "Please input before firing.")
-        return
+    mk = "\n[" + str(a) + ".wait, " + str(b) + ".inside, " + str(c) + ".done]";
+    text_area.config(state = "normal")
+    text_area.insert(INSERT, mk)
+    text_area.config(state = "disabled")
 
+def changeclick():
     inside.config(state = "normal")
     done.config(state = "normal")
 
-    a = int(inside.get("1.0", END))
-    b = int(done.get("1.0", END))
+    b = int(inside.get("1.0", END))
 
-    if a == 0:
-        messagebox.showerror("Error!!!", "There are no tokens in the place change.")
+    if b == 0:
+        messagebox.showerror("Error!!!", "There are no tokens in place [change].")
         return
+
+    c = int(done.get("1.0", END))
+    a = int(wait.get("1.0", END))
 
     inside.delete("1.0", END)
     done.delete("1.0", END)
 
-    inside.insert(END, a - 1)
-    done.insert(END, b + 1)
+    b = b - 1
+    c = c + 1
+
+    inside.tag_configure("tag_name", justify='center')
+    inside.insert(END, b)
+    inside.tag_add("tag_name", "1.0", "end")
+
+    done.tag_configure("tag_name", justify='center')
+    done.insert(END, c)
+    done.tag_add("tag_name", "1.0", "end")
 
     inside.config(state = "disabled")
     done.config(state = "disabled")
+
+    mk = "\n[" + str(a) + ".wait, " + str(b) + ".inside, " + str(c) + ".done]";
+    text_area.config(state = "normal")
+    text_area.insert(INSERT, mk)
+    text_area.config(state = "disabled")
 
 def enter():
     a = input_wait.get()
@@ -97,6 +121,10 @@ def enter():
     input_inside.delete(0, END)
     input_done.delete(0, END)
 
+    if a.isnumeric() == False or b.isnumeric() == False or c.isnumeric() == False:
+        messagebox.showerror("Error!!!", "Your input is not a positive number.")
+        return
+
     wait.config(state = "normal")
     inside.config(state = "normal")
     done.config(state = "normal")
@@ -105,32 +133,42 @@ def enter():
     inside.delete("1.0", END)
     done.delete("1.0", END)
 
-    if a.isnumeric() == False or b.isnumeric() == False or c.isnumeric() == False:
-        messagebox.showerror("Exception!!!", "Your input is not a number.")
-        return
-
     a = int(a)
     b = int(b)
     c = int(c)
-
-    if a < 0 or b < 0 or c < 0:
-        messagebox.showerror("Exception!!!", "The numbers of tokens must be positive.")
-        return
     
+    wait.tag_configure("tag_name", justify='center')
     wait.pack()
     wait.insert(END, a)
+    wait.tag_add("tag_name", "1.0", "end")
     wait.config(state = "disabled")
     wait.place(x = 78, y = 90)
 
+    inside.tag_configure("tag_name", justify='center')
     inside.pack()
     inside.insert(END, b)
+    inside.tag_add("tag_name", "1.0", "end")
     inside.config(state = "disabled")
     inside.place(x = 478, y = 90)
 
+    done.tag_configure("tag_name", justify='center')
     done.pack()
     done.insert(END, c)
+    done.tag_add("tag_name", "1.0", "end")
     done.config(state = "disabled")
     done.place(x = 878, y = 90)
+
+    mk = "[" + str(a) + ".wait, " + str(b) + ".inside, " + str(c) + ".done]";
+    text_area.config(state = "normal")
+    text_area.delete("1.0", END)
+    text_area.insert(INSERT, "Marking obtained after firing:\n")
+    text_area.insert(INSERT, mk)
+    text_area.config(state = "disabled")
+
+def clear():
+    text_area.config(state = "normal")
+    text_area.delete("1.0", END)
+    text_area.config(state = "disabled")
 
 wait = Text(frame, font = "Arial 10", bg = "silver", width = 6, height = 1, fg = "navy")
 inside = Text(frame, font = "Arial 10", bg = "silver", width = 6, height = 1, fg = "navy")
@@ -173,6 +211,15 @@ input_done.place(x = 70, y = 155)
 
 Enter_token = Button(frame_input, text = "Enter", font = "Arial 14", fg = "royalblue", bg = "lavender", padx = 5, pady = 5, command = enter)
 Enter_token.pack()
-Enter_token.place(x = 360, y = 190)
+Enter_token.place(x = 380, y = 185)
+
+text_area = scrolledtext.ScrolledText(frame2, wrap = tk.WORD, width = 54, height = 12, font = ("Times New Roman", 13))
+text_area.pack()
+text_area.place(y = 0)
+text_area.config(state = "disabled")
+
+Clear = Button(frame2, text = "Clear", font = "Arial 14", fg = "royalblue", bg = "lavender", padx = 5, pady = 5, command = clear)
+Clear.pack()
+Clear.place(x = 425, y = 185)
 
 window.mainloop()
